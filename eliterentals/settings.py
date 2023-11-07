7a582487deddf1a3d59505632e421d1c2fb1a905
PrasanMadhikarmi <prasanmadhikarmi7@gmail.com>
2023-11-07 11:34:55 +0545

    working on displaying vehicles

--- eliterentals/settings.py
+++ eliterentals/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'services.apps.ServicesConfig'
 ]
 
 MIDDLEWARE = [
---
+++ services/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+from accounts.models import VehicleRegistration
+# Create your models here.
+
+class bookInstantly(models.Model):
+    st=(
+    ("Processing","Processing"),
+    ("Accepted","Accepted"),
+    ("Cancelled","Cancelled"),
+    ("Decline","Decline"),
+    ("Done","Done"),
+    )
+
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=100)
+    number = models.CharField(max_length=10)
+    pickAddress = models.CharField(max_length=255)
+    pickDate = models.DateField()
+    pickTime = models.TimeField()
+    dropAddress = models.CharField(max_length=255)
+    dropDate = models.DateField()
+    dropTime = models.TimeField()
+    status = models.CharField(max_length=150, choices=st, default='Processing')
+    vehicle_id = models.ForeignKey(VehicleRegistration, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+    
+
+    def __str__(self):
+        return f'{self.id} - {self.status}'
