9f2abeb96c9394c0395224b19b686673b3e3602f
PrasanMadhikarmi <prasanmadhikarmi7@gmail.com>
2023-11-08 11:25:13 +0545

    working on vehicle detail page

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,7 @@ class UserAddress(models.Model):
     address = models.CharField(max_length=150)
     street = models.CharField(max_length=150)
     postalcode = models.CharField(max_length=10)
+       
     city = models.CharField(max_length=150)
     country = models.CharField(max_length=150, choices=country_choices, default='Nepal')
     
